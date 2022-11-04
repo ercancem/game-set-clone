a728d15e208b02e783f2277fd9b8d201ba006733
ercancem <ercan.cem@gmail.com>
2022-11-04 21:27:37 +0300

    JS

--- script.js
+++ script.js
@@ -14,32 +14,31 @@ function renderCircle(node, radius = 12, color, fill) {
     const circle2 = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
     const circle3 = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
 
-    // svg.setAttribute('viewbox', `0 0 ${radius*2} ${radius*2}`);
     svg.setAttribute('width', `${radius * 2}px`);
     svg.setAttribute('height', `${radius * 2}px`);
+    svg.setAttribute('viewBox', '0 0 60 60')
     svg.classList.add('post-icon');
 
-    circle1.setAttribute('cx', radius);
-    circle1.setAttribute('cy', radius);
-    circle1.setAttribute('r', radius);
-    // circle1.setAttribute('fill', color);
+    circle1.setAttribute('cx', 30);
+    circle1.setAttribute('cy', 30);
+    circle1.setAttribute('r', 30);
     circle1.setAttribute('stroke', 'none');
     circle1.classList.add(color);
     svg.appendChild(circle1);
 
-    if (fill === (1 || 2)) {
-        circle2.setAttribute('cx', radius);
-        circle2.setAttribute('cy', radius);
-        circle2.setAttribute('r', radius * 0.5);
+    if (fill > 0) {
+        circle2.setAttribute('cx', 30);
+        circle2.setAttribute('cy', 30);
+        circle2.setAttribute('r', 20);
         circle2.classList.add("white");
         circle2.setAttribute('stroke', 'none');
         svg.appendChild(circle2);
     }
 
     if (fill === 2) {
-        circle3.setAttribute('cx', radius);
-        circle3.setAttribute('cy', radius);
-        circle3.setAttribute('r', radius * 0.25);
+        circle3.setAttribute('cx', 30);
+        circle3.setAttribute('cy', 30);
+        circle3.setAttribute('r', 10);
         circle3.classList.add(color);
         circle3.setAttribute('stroke', 'none');
         svg.appendChild(circle3);
@@ -54,34 +53,36 @@ function renderSquare(node, side = 12, color, fill) {
     const rect2 = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
     const rect3 = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
 
-    // svg.setAttribute('viewbox', `0 0 ${radius*2} ${radius*2}`);
-    svg.setAttribute('width', `${side*2}px`);
-    svg.setAttribute('height', `${side*2}px`);
+    svg.setAttribute('width', `${side * 2}px`);
+    svg.setAttribute('height', `${side * 2}px`);
+    svg.setAttribute('viewBox', '0 0 60 60')
     svg.classList.add('post-icon');
 
     rect1.setAttribute('x', 0);
     rect1.setAttribute('y', 0);
-    rect1.setAttribute('width', side*2);
-    rect1.setAttribute('height', side*2);
+    rect1.setAttribute('width', 60);
+    rect1.setAttribute('height', 60);
     rect1.classList.add(color);
     rect1.setAttribute('stroke', 'none');
     svg.appendChild(rect1);
 
-    if (fill === (1 || 2)) {
-        rect2.setAttribute('x', (side/2));
-        rect2.setAttribute('y', (side/2));
-        rect2.setAttribute('width', side);
-        rect2.setAttribute('height', side);
+    if (fill > 0) {
+        console.log("Inside first if")
+        rect2.setAttribute('x', 10);
+        rect2.setAttribute('y', 10);
+        rect2.setAttribute('width', 40);
+        rect2.setAttribute('height', 40);
         rect2.classList.add("white");
         rect2.setAttribute('stroke', 'none');
         svg.appendChild(rect2);
     }
 
-    if (fill === (2)) {
-        rect3.setAttribute('x', (side / 4));
-        rect3.setAttribute('y', (side / 4));
-        rect3.setAttribute('width', side / 2);
-        rect3.setAttribute('height', side / 2);
+    if (fill === 2) {
+        console.log("Inside second if")
+        rect3.setAttribute('x', 20);
+        rect3.setAttribute('y', 20);
+        rect3.setAttribute('width', 20);
+        rect3.setAttribute('height', 20);
         rect3.classList.add(color);
         rect3.setAttribute('stroke', 'none');
         svg.appendChild(rect3);
@@ -96,8 +97,8 @@ function renderDiamond(node, side = 12, color, fill) {
     const path2 = document.createElementNS("http://www.w3.org/2000/svg", 'path');
     const path3 = document.createElementNS("http://www.w3.org/2000/svg", 'path');
 
-    svg.setAttribute('width', `${side*2}px`);
-    svg.setAttribute('height', `${side*2}px`);
+    svg.setAttribute('width', `${side * 2}px`);
+    svg.setAttribute('height', `${side * 2}px`);
     svg.setAttribute('viewBox', '0 0 60 60')
     svg.classList.add('post-icon');
 
@@ -106,14 +107,14 @@ function renderDiamond(node, side = 12, color, fill) {
     path1.setAttribute('stroke', 'none');
     svg.appendChild(path1);
 
-    if (fill === (1 || 2)) {
-        path2.setAttribute('d','m10 30 20-20 20 20-20 20Z');
+    if (fill > 0) {
+        path2.setAttribute('d', 'm10 30 20-20 20 20-20 20Z');
         path2.classList.add("white");
         path2.setAttribute('stroke', 'none');
         svg.appendChild(path2);
     }
 
-    if (fill === (2)) {
+    if (fill === 2) {
         path3.setAttribute('d', 'm20 30 10-10 10 10-10 10Z');
         path3.classList.add(color);
         path3.setAttribute('stroke', 'none');
@@ -141,30 +142,36 @@ for (let i = 0; i < 81; i++) {
 
 shuffleArray(tileSet);
 
-function tileBuilder() {
-    const newTile = tileSet.pop();
-    console.log(newTile);
-    tilesOnBoard.push(newTile);
-    const tilePos = document.getElementById("row-0-column-0");
-    for (let i = 0; i <= newTile[2]; i++) {
+function tileDecorator(tileId, tileArray) {
+    console.log(tileId);
+    for (let i = 0; i <= tileArray[2]; i++) {
+        const tilePos = document.getElementById("tile-no-" + tileId);
         let shape = document.createElement("div");
-        if (newTile[3] === 0) {
-            renderCircle(shape, 12, COLORS[newTile[0]], newTile[1]);
+        if (tileArray[3] === 0) {
+            renderCircle(shape, 12, COLORS[tileArray[0]], tileArray[1]);
             tilePos.append(shape);
             continue
         }
 
-        if (newTile[3] === 1) {
-            renderSquare(shape, 12, COLORS[newTile[0]], newTile[1]);
+        if (tileArray[3] === 1) {
+            renderSquare(shape, 12, COLORS[tileArray[0]], tileArray[1]);
             tilePos.append(shape);
             continue
         }
         else {
-            renderDiamond(shape, 12, COLORS[newTile[0]], newTile[1]);
+            renderDiamond(shape, 12, COLORS[tileArray[0]], tileArray[1]);
             tilePos.append(shape);
         }
+    }
 
+    // return tile;
+}
 
+function tileBuilder() {
+    for (let i = 0; i < 12; i++) {
+        let newTile = tileSet.pop();
+        tilesOnBoard.push(newTile);
+        tileDecorator(i, newTile);
     }
 }
 
@@ -238,15 +245,17 @@ function createBoard() {
 
 function createTiles() {
     const board = document.getElementById("board");
-
+    let tileCounter = 0;
     for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
         const tileRow = document.createElement("div");
+        // tileRow.setAttribute("id", "row-" + rowIndex);
         tileRow.setAttribute("id", "row-" + rowIndex);
         tileRow.classList.add("row-container");
         board.append(tileRow);
         for (let clmIndex = 0; clmIndex < 4; clmIndex++) {
             let rowElement = document.createElement("div");
-            rowElement.setAttribute("id", "row-" + rowIndex + "-column-" + clmIndex);
+            rowElement.setAttribute("id", "tile-no-" + tileCounter);
+            tileCounter++;
             rowElement.classList.add("tile");
             tileRow.append(rowElement);
         }
